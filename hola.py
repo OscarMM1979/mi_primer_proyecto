f7fd585533d4d0c5380f44ddf532847f4a83805a
OscarMM1979 <oscar.mendez64@unisalamanca.edu.co>
2025-11-12 16:27:37 -0500

    Segunda versión: se muestra la longitud del nombre

--- hola.py
+++ hola.py
@@ -4,3 +4,8 @@
 
 nombre = input("¿Cómo te llamas? ")
 print(f"¡Hola, {nombre}! Bienvenido a tu primer proyecto con Git y Python.")
+# hola.py
+# Versión mejorada
+nombre = input("¿Cómo te llamas? ")
+print(f"¡Hola, {nombre}! Bienvenido a tu primer proyecto con Git y Python.")
+print(f"Tu nombre tiene {len(nombre)} letras.")
